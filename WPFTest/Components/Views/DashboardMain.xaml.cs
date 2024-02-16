f6a364d6feb7bc4cc0b5795a2a759cb22b7a2792
Ushal Shrestha <ushal_bindukar@ymail.com>
2024-02-16 15:33:41 +0545

    dashboardview added

---
+++ WPFTest/Components/Views/DashboardMain.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+
+namespace OxpanPosDesktopUI.Views
+{
+    /// <summary>
+    /// Interaction logic for DashboardMain.xaml
+    /// </summary>
+    public partial class DashboardMain : Page
+    {
+        public DashboardMain()
+        {
+            InitializeComponent();
+
+            Frame topbarMenu = new Frame();
+            topbarMenu.Navigate(new Topbar());
+        }
+    }
+}
