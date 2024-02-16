8848dbabe3e24ccd91d1e5ac8c2858bbbbc337d4
Ushal Shrestha <ushal_bindukar@ymail.com>
2024-02-16 15:36:01 +0545

    tobarview added

---
+++ WPFTest/Components/Views/Topbar.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Controls;
+
+namespace OxpanPosDesktopUI.Views
+{
+  /// <summary>
+  /// Interaction logic for Topbar.xaml
+  /// </summary>
+  public partial class Topbar : Page
+  {
+    public Topbar()
+    {
+      InitializeComponent();
+    }
+
+    private void btnLogOut_Click(object sender, System.Windows.RoutedEventArgs e)
+    {
+
+    }
+  }
+}
