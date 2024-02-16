4ba2207dc6536e81149000a76b1b942016a9a5be
Ushal Shrestha <ushal_bindukar@ymail.com>
2024-02-16 15:38:55 +0545

    sidebarmenu view added

---
+++ WPFTest/Components/Views/SidebarMenu.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace OxpanPosDesktopUI.Views
+{
+    /// <summary>
+    /// Interaction logic for SidebarMenu.xaml
+    /// </summary>
+    public partial class SidebarMenu : Page
+    {
+        public SidebarMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            if (button != null)
+            {
+                string contentName = button.Name.ToString() + "Content";
+                StackPanel contentPanel = FindName(contentName) as StackPanel;
+                if (contentPanel != null)
+                {
+                    contentPanel.Visibility = contentPanel.Visibility == Visibility.Collapsed ? Visibility.Visible : Visibility.Collapsed;
+                }
+            }
+        }
+    }
+}
